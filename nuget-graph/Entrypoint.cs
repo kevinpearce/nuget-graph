d3d11f1c4a9f3390e5e78a31fd8efb80ca16d5b0
Kevin Pearce <kevinjohnpearce@googlemail.com>
2025-01-26 13:24:11 +0000

    remove local testing statement

--- nuget-graph/Entrypoint.cs
+++ nuget-graph/Entrypoint.cs
@@ -4,8 +4,7 @@ public class Entrypoint : IEntrypoint
 {
     public Task Run(string[] args)
     {
-        // var rootDirectory = Directory.GetCurrentDirectory();
-        var rootDirectory = "../../../example"; //local testing
+        var rootDirectory = Directory.GetCurrentDirectory();
         
         var graph = args.Length == 0 ? new Graph(rootDirectory) : new Graph(rootDirectory, args);
         
